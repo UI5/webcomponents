fc3ec8f50b093ea142ff3fdcfcdbd95807dc4540
ilhan007 <ilhan.myumyun@sap.com>
2025-09-24 11:20:11 +0300

    ci: run main cypress tests in 4 suites

--- packages/main/cypress.config.js
+++ packages/main/cypress.config.js
@@ -3,11 +3,17 @@ import cypressConfig from "@ui5/cypress-internal/cypress.config.js";
 const suites = {
 	SUITE1: [
 		"**/specs/base/*.cy.{jsx,tsx}",
-		"**/specs/[A-I]*.cy.{js,jsx,ts,tsx}",
+		"**/specs/[A-D]*.cy.{js,jsx,ts,tsx}",
 	],
 	SUITE2: [
-		"**/specs/[^A-I]*.cy.{js,jsx,ts,tsx}",
+		"**/specs/[E-L]*.cy.{js,jsx,ts,tsx}",
 	],
+    SUITE3: [
+        "**/specs/[M-R]*.cy.{js,jsx,ts,tsx}",
+    ],
+    SUITE4: [
+        "**/specs/[S-Z]*.cy.{js,jsx,ts,tsx}",
+    ],
 };
 
 cypressConfig.component.specPattern = suites[process.env.TEST_SUITE] || ["**/specs/**/*.cy.{js,ts,jsx,tsx}"];
