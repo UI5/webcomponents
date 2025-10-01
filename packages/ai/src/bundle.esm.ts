72b644354e9c9307fa133110bf4afae8102bcc43
Stefan Dimitrov <stefan.dimitrov02@sap.com>
2025-10-01 19:06:58 +0300

    feat(ui5-ai-input): resolve conflicts

--- packages/ai/src/bundle.esm.ts
+++ packages/ai/src/bundle.esm.ts
@@ -7,11 +7,7 @@ import "./Assets.js";
 import Button from "./Button.js";
 import ButtonState from "./ButtonState.js";
 import PromptInput from "./PromptInput.js";
-<<<<<<< Updated upstream
-import AITextArea from "./AITextArea.js";
-=======
 import AITextArea from "./TextArea.js";
 import AIInput from "./AIInput.js";
->>>>>>> Stashed changes
 
 export default testAssets;
