f7cd89156f8dfc3cf75935f5f40c27e1ba66f351
Georgi Damyanov <georgi.damyanov@sap.com>
2025-10-02 20:29:05 +0300

    refactor: add jsdoc

--- packages/main/src/types/SegmentedButtonContentMode.ts
+++ packages/main/src/types/SegmentedButtonContentMode.ts
@@ -1,6 +1,18 @@
+/**
+ * Different SegmentedButton items sizing modes.
+ * @public
+ */
 enum SegmentedButtonContentMode {
+    /**
+	 * Each item fits its content and extra space is placed after the last item.
+	 * @public
+	 */
     ContentFit = "ContentFit",
+    /** /**
+	 * All items are sized equally to fill the available space.
+	 * @public
+	 */
     EqualSized = "EqualSized",
-};
+}
 
 export default SegmentedButtonContentMode;
