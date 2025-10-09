e1f1819c45ee19d9af9c2a8a5a4300e516bf087a
Georgi Damyanov <georgi.damyanov@sap.com>
2025-10-09 12:15:57 +0300

    fix: handle space on keyup properly

--- packages/main/src/SplitButton.ts
+++ packages/main/src/SplitButton.ts
@@ -286,6 +286,7 @@ class SplitButton extends UI5Element {
 	}
 
 	_onKeyDown(e: KeyboardEvent) {
+		console.log(`keydown ${e.key}`);
 		if (this._isArrowKeyAction(e)) {
 			this._handleArrowButtonAction(e);
 			this._activeArrowButton = true;
@@ -310,6 +311,7 @@ class SplitButton extends UI5Element {
 	}
 
 	_onKeyUp(e: KeyboardEvent) {
+		console.log(`keyup ${e.key}`);
 		const target = e.target as Button;
 		if (this._isArrowKeyAction(e)) {
 			e.preventDefault();
@@ -321,8 +323,14 @@ class SplitButton extends UI5Element {
 			e.preventDefault();
 			e.stopPropagation();
 			this._textButtonActive = false;
-			if (!this._shiftOrEscapePressedDuringSpace && target !== this.arrowButton) { // Do not fire click if Arrow button is focused by mouse and Space is pressed afterwards
-				this._fireClick();
+			if (!this._shiftOrEscapePressedDuringSpace) { // Do not fire click if Arrow button is focused by mouse and Space is pressed afterwards
+				if (target !== this.arrowButton) {
+					console.log("fire click");
+					this._fireClick();
+				} else {
+					this._fireArrowClick();
+					console.log("fire arrow click");
+				}
 			}
 
 			this._shiftOrEscapePressedDuringSpace = false;
@@ -422,15 +430,14 @@ class SplitButton extends UI5Element {
 		e.preventDefault();
 		const target = e.target as Button;
 
-		if (this.arrowButton && target === this.arrowButton) {
-			this._activeArrowButton = true;
-			this._fireArrowClick();
-			return;
-		}
-
-		this._textButtonActive = true;
-
 		if (isEnter(e)) {
+			if (this.arrowButton && target === this.arrowButton) {
+				this._activeArrowButton = true;
+				this._fireArrowClick();
+				return;
+			}
+
+			this._textButtonActive = true;
 			this._fireClick(e);
 			return;
 		}
