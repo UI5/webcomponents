d72784b0d36672f46fcd76ec3bf1580531f83593
Nayden Naydenov <nnaydenow.work@gmail.com>
2025-11-25 16:03:15 +0200

    chore: fixes

--- packages/tools/lib/chokidar/chokidar.js
+++ packages/tools/lib/chokidar/chokidar.js
@@ -1,14 +1,13 @@
 const chokidar = require('chokidar');
 const { exec } = require("child_process");
 
-
 const main = async (argv) => {
 	if (argv.length < 4) {
 		console.error("Please provide a file pattern to watch and a command to execute on changes.");
 		console.error("<file-pattern> <command>");
 		process.exit(1);
 	}
-	console.log(argv);
+
 	const filePattern = argv[2];
 	const command = argv.slice(3).join(' ');
 
