aab984918714ff51bb308623aa2841480739c7b3
Stefan Dimitrov <115184100+StefanDimitrov04@users.noreply.github.com>
2025-12-04 09:45:11 +0200

    Fix comment formatting in SearchItemShowMore.ts
    Corrected comment formatting for clarity.

--- packages/fiori/src/SearchItemShowMore.ts
+++ packages/fiori/src/SearchItemShowMore.ts
@@ -62,7 +62,7 @@ class SearchItemShowMore extends ListItemBase {
 	}
 	/**
 	 * Specifies the number of additional items available to show.
-If no value is defined, the control shows "Show more" (without any counter). 
+If no value is defined, the control shows "Show more" (without any counter).
 If a number is provided, it displays "Show more (N)", where N is that number.
 	 * @public
 	 * @default undefined
