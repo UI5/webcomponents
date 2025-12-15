97d23ffad7a737cca59fca6a1e0e6bb0abaa042b
Diana Pazheva <diana.pazheva@sap.com>
2025-12-15 11:54:27 +0200

    feat(ui5-list): update sample

--- packages/website/docs/_samples/main/List/StickyHeader/main.js
+++ packages/website/docs/_samples/main/List/StickyHeader/main.js
@@ -1,4 +1,4 @@
 import "@ui5/webcomponents/dist/List.js";
 import "@ui5/webcomponents/dist/ListItemStandard.js";
 
-import "@ui5/webcomponents-icons/dist/product.js";
+import "@ui5/webcomponents-icons/dist/nutrition-activity.js";
