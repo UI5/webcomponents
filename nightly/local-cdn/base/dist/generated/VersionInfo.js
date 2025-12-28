13bf1e54b56ccaa2defacfd98437b52fa26a76e6
ilhan007 <ilhan007@users.noreply.github.com>
2025-12-28 00:25:12 +0000

    Deploying to gh-pages from @ UI5/webcomponents@7bd25a1267953fe0b38ff07839c30563892aaf97 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.2",
     isNext: false,
-    buildTime: 1766794675,
+    buildTime: 1766881247,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
