2dd06ed2512e5fc9bf72a753f2735fd8ee1dd0fa
Nayden Naydenov <nnaydenow.work@gmail.com>
2026-01-05 22:43:23 +0200

    fix(tools): generation of json imports for themes

--- packages/tools/lib/generate-json-imports/themes.js
+++ packages/tools/lib/generate-json-imports/themes.js
@@ -21,12 +21,20 @@ const generate = async (argv) => {
 		return matches ? dir : undefined;
 	}).filter(key => !!key && allThemes.includes(key));
 
+	// Themes that doesn't exist on the file system
+	const missingThemes = allThemes.filter(theme => !themesOnFileSystem.includes(theme));
+
 	const packageName = JSON.parse(await fs.readFile("package.json")).name;
 
-	const availableThemesArray = `[${themesOnFileSystem.map(theme => `"${theme}"`).join(", ")}]`;
+	// Contain all supported themes from assets-meta.js
+	const availableThemesArray = `[${allThemes.map(theme => `"${theme}"`).join(", ")}]`;
+
 	const dynamicImportLines = themesOnFileSystem.map(theme => `\t\tcase "${theme}": return (await import(/* webpackChunkName: "${packageName.replace("@", "").replace("/", "-")}-${theme.replace("_", "-")}-parameters-bundle" */"../assets/themes/${theme}/parameters-bundle.css.json")).default;`).join("\n");
 	const dynamicImportJSONAttrLines = themesOnFileSystem.map(theme => `\t\tcase "${theme}": return (await import(/* webpackChunkName: "${packageName.replace("@", "").replace("/", "-")}-${theme.replace("_", "-")}-parameters-bundle" */"../assets/themes/${theme}/parameters-bundle.css.json", {with: { type: 'json'}})).default;`).join("\n");
 	const fetchMetaResolveLines = themesOnFileSystem.map(theme => `\t\tcase "${theme}": return (await fetch(new URL("../assets/themes/${theme}/parameters-bundle.css.json", import.meta.url))).json();`).join("\n");
+
+	// Lines for missing themes to avoid runtime errors. By default parameters-bundle.css.json returns a strings.
+	const missingThemesLines = missingThemes.map(theme => `\t\tcase "${theme}": return "";`).join("\n");
 
 	// dynamic imports file content
 	const contentDynamic = function (lines) {
@@ -55,9 +63,9 @@ ${availableThemesArray}
 
 	await fs.mkdir(path.dirname(outputFileDynamic), { recursive: true });
 	return Promise.all([
-		fs.writeFile(outputFileDynamic, contentDynamic(dynamicImportLines)),
-		fs.writeFile(outputFileDynamicImportJSONAttr, contentDynamic(dynamicImportJSONAttrLines)),
-		fs.writeFile(outputFileFetchMetaResolve, contentDynamic(fetchMetaResolveLines)),
+		fs.writeFile(outputFileDynamic, contentDynamic(dynamicImportLines.concat("\n", missingThemesLines))),
+		fs.writeFile(outputFileDynamicImportJSONAttr, contentDynamic(dynamicImportJSONAttrLines.concat("\n", missingThemesLines))),
+		fs.writeFile(outputFileFetchMetaResolve, contentDynamic(fetchMetaResolveLines.concat("\n", missingThemesLines))),
 	]).
 		then(() => {
 			console.log("Generated themes JSON imports.");
