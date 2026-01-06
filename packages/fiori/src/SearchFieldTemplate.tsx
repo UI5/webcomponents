32ac27fcb61c814b00761c0deee908e581fad376
Dobrin Dimchev <dobrin.dimchev@sap.com>
2026-01-06 14:15:48 +0200

    fix conflicts

--- packages/fiori/src/SearchFieldTemplate.tsx
+++ packages/fiori/src/SearchFieldTemplate.tsx
@@ -39,10 +39,12 @@ export default function SearchFieldTemplate(this: SearchField, options?: SearchF
 									onChange={this._handleScopeChange}
 									class="sapUiSizeCompact ui5-search-field-select"
 									accessibleName={this._translations.scope}
-									tooltip={this._translations.scope}>
+									tooltip={this._translations.scope}
+									value={this.scopeValue}
+								>
 									{this.scopes.map(scopeOption => (
 										<Option
-											selected={scopeOption.selected}
+											value={scopeOption.value}
 											data-ui5-stable={scopeOption.stableDomRef}
 											ref={this.captureRef.bind(scopeOption)}
 										>{scopeOption.text}
@@ -65,6 +67,8 @@ export default function SearchFieldTemplate(this: SearchField, options?: SearchF
 							role="searchbox"
 							aria-description={this.accessibleDescription}
 							aria-label={this.accessibleName || this._translations.searchFieldAriaLabel}
+							aria-autocomplete="both"
+							aria-controls="ui5-search-list"
 							value={this.value}
 							placeholder={this.placeholder}
 							data-sap-focus-ref
