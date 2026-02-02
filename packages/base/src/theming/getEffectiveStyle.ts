3d3d68e1e4d83e13676823a02230eaeeaa2d7e80
Nayden Naydenov <nnaydenow.work@gmail.com>
2026-02-02 09:57:57 +0200

    chore: clean busy indicator related enhancements

--- packages/base/src/theming/getEffectiveStyle.ts
+++ packages/base/src/theming/getEffectiveStyle.ts
@@ -1,8 +1,6 @@
 import { getCustomCSS, attachCustomCSSChange } from "./CustomStyle.js";
 import getStylesString from "./getStylesString.js";
-import { getFeature } from "../FeaturesRegistry.js";
 import type UI5Element from "../UI5Element.js";
-import type OpenUI5Enablement from "../features/OpenUI5Enablement.js";
 
 const effectiveStyleMap = new Map<string, string>();
 
@@ -13,19 +11,12 @@ attachCustomCSSChange((tag: string) => {
 const getEffectiveStyle = (ElementClass: typeof UI5Element) => {
 	const tag = ElementClass.getMetadata().getTag();
 	const key = `${tag}_normal`;
-	const openUI5Enablement = getFeature<typeof OpenUI5Enablement>("OpenUI5Enablement");
 
 	if (!effectiveStyleMap.has(key)) {
-		let busyIndicatorStyles = "";
-
-		if (openUI5Enablement) {
-			busyIndicatorStyles = getStylesString(openUI5Enablement.getBusyIndicatorStyles());
-		}
-
 		const customStyle = getCustomCSS(tag) || "";
 		const builtInStyles = getStylesString(ElementClass.styles);
 
-		const effectiveStyle = `${builtInStyles} ${customStyle} ${busyIndicatorStyles}`;
+		const effectiveStyle = `${builtInStyles} ${customStyle}`;
 		effectiveStyleMap.set(key, effectiveStyle);
 	}
 
