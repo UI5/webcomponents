f4910d525bdc38858ea642317fe6d7bc6f0494bd
Nayden Naydenov <nnaydenow.work@gmail.com>
2026-02-02 09:58:37 +0200

    chore: remove file

--- packages/base/src/features/OpenUI5Enablement.ts
+++
@@ -1,7 +0,0 @@
-import { registerFeature } from "../FeaturesRegistry.js";
-
-class OpenUI5Enablement {}
-
-registerFeature("OpenUI5Enablement", OpenUI5Enablement);
-
-export default OpenUI5Enablement;
