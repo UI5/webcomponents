55569e8f82e1c69e7df5f4a21ff83fd88123e740
Milen Karmidzhanov <milen.karmidzhanov@sap.com>
2026-02-04 15:23:13 +0200

    feat(ui5-tokenizer/ui5-multi-input/ui5-multi-combobox): align mobile popover title with spec

--- packages/main/src/Input.ts
+++ packages/main/src/Input.ts
@@ -51,7 +51,6 @@ import {
 	deregisterUI5Element,
 	getEffectiveAriaDescriptionText,
 	getAllAccessibleDescriptionRefTexts,
-	getEffectiveAriaLabelText,
 } from "@ui5/webcomponents-base/dist/util/AccessibilityTextsHelper.js";
 import { getCaretPosition, setCaretPosition } from "@ui5/webcomponents-base/dist/util/Caret.js";
 import getActiveElement from "@ui5/webcomponents-base/dist/util/getActiveElement.js";
@@ -1736,7 +1735,7 @@ class Input extends UI5Element implements SuggestionComponent, IFormInputElement
 	}
 
 	get _headerTitleText() {
-		return getEffectiveAriaLabelText(this) || Input.i18nBundle.getText(INPUT_SUGGESTIONS_TITLE);
+		return Input.i18nBundle.getText(INPUT_SUGGESTIONS_TITLE);
 	}
 
 	get _suggestionsOkButtonText() {
