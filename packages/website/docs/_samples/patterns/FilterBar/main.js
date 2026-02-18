5525b2bbb874c852686e9fe6fa37b97305813f24
ilhan007 <ilhan.myumyun@sap.com>
2026-02-18 13:18:49 +0200

    docs: add new pattern

---
+++ packages/website/docs/_samples/patterns/FilterBar/main.js
@@ -0,0 +1,13 @@
+import "@ui5/webcomponents/dist/Button.js";
+import "@ui5/webcomponents/dist/Input.js";
+import "@ui5/webcomponents/dist/features/InputSuggestions.js";
+import "@ui5/webcomponents/dist/SuggestionItemCustom.js";
+import "@ui5/webcomponents/dist/SuggestionItemGroup.js";
+import "@ui5/webcomponents/dist/Tag.js";
+import "@ui5/webcomponents/dist/Text.js";
+import "@ui5/webcomponents/dist/Icon.js";
+import "@ui5/webcomponents-icons/dist/ai.js";
+import "@ui5/webcomponents-icons/dist/favorite.js";
+import "@ui5/webcomponents-icons/dist/add-favorite.js";
+import "@ui5/webcomponents-icons/dist/decline.js";
+
