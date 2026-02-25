714007c8e13c9bb40c00bd250f9118bc2d598794
Georgi Damyanov <georgi.damyanov@sap.com>
2026-02-25 11:32:00 +0200

    fix: add year range button

--- packages/main/src/CalendarTemplate.tsx
+++ packages/main/src/CalendarTemplate.tsx
@@ -49,11 +49,14 @@ export default function CalendarTemplate(this: Calendar) {
 														"ui5-calheader-arrowbtn": true,
 														"ui5-calheader-arrowbtn-disabled": this._previousButtonDisabled,
 													}}
+													part="calendar-header-arrow-button"
 													role="button"
 													onMouseDown={this.onPrevButtonClick}
 													tabindex={this._previousButtonDisabled ? -1 : 0}
 													title={this.accInfo.tooltipPrevButton}
 													aria-label={this.accInfo.ariaLabelPrevButton}
+													aria-description={this.accInfo.ariaLabelPrevButton}
+													aria-keyshortcuts={this.accInfo.keyShortcutPrevButton}
 												>
 													<Icon class="ui5-calheader-arrowicon" name={slimArrowLeft}/>
 												</div>
@@ -63,8 +66,14 @@ export default function CalendarTemplate(this: Calendar) {
 												<div
 													data-ui5-cal-header-btn-month
 													class="ui5-calheader-arrowbtn ui5-calheader-middlebtn"
+													part="calendar-header-middle-button"
+													hidden={this._isHeaderMonthButtonHidden}
 													tabindex={0}
 													role="button"
+													aria-label={this.accInfo.ariaLabelMonthButton}
+													aria-description={this.accInfo.ariaLabelMonthButton}
+													title={this.accInfo.tooltipMonthButton}
+													aria-keyshortcuts={this.accInfo.keyShortcutMonthButton}
 													onClick={this.onHeaderMonthButtonPress}
 													onKeyDown={this.onMonthButtonKeyDown}
 													onKeyUp={this.onMonthButtonKeyUp}
@@ -78,15 +87,41 @@ export default function CalendarTemplate(this: Calendar) {
 												<div
 													data-ui5-cal-header-btn-year
 													class="ui5-calheader-arrowbtn ui5-calheader-middlebtn"
+													part="calendar-header-middle-button"
+													hidden={this._isHeaderYearButtonHidden}
 													tabindex={0}
 													role="button"
+													aria-label={this.accInfo.ariaLabelYearButton}
+													aria-description={this.accInfo.ariaLabelYearButton}
 													onClick={this.onHeaderYearButtonPress}
 													onKeyDown={this.onYearButtonKeyDown}
 													onKeyUp={this.onYearButtonKeyUp}
+													title={this.accInfo.tooltipYearButton}
+													aria-keyshortcuts={this.accInfo.keyShortcutYearButton}
 												>
 													<span>{headerText.yearText}</span>
 													{this.hasSecondaryCalendarType && headerText.secondYearText &&
 														<span class="ui5-calheader-btn-sectext">{headerText.secondYearText}</span>
+													}
+												</div>
+												<div
+													data-ui5-cal-header-btn-year-range
+													class="ui5-calheader-arrowbtn ui5-calheader-middlebtn"
+													part="calendar-header-middle-button"
+													hidden={this._isHeaderYearRangeButtonHidden}
+													tabindex={0}
+													role="button"
+													aria-label={this.accInfo.ariaLabelYearRangeButton}
+													aria-description={this.accInfo.ariaLabelYearRangeButton}
+													title={this.accInfo.tooltipYearRangeButton}
+													aria-keyshortcuts={this.accInfo.keyShortcutYearRangeButton}
+													onClick={this.onHeaderYearRangeButtonPress}
+													onKeyDown={this.onYearRangeButtonKeyDown}
+													onKeyUp={this.onYearRangeButtonKeyUp}
+												>
+													<span>{this._headerYearRangeButtonText}</span>
+													{this.hasSecondaryCalendarType &&
+														<span class="ui5-calheader-btn-sectext">{this._headerYearRangeButtonTextSecType}</span>
 													}
 												</div>
 											</div>
