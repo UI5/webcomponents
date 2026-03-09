4c32664c342bdba8b21b05f1ae1de11b75055596
Lidiya Georgieva <lidiya.georgieva@sap.com>
2026-03-09 16:19:04 +0200

    feat(ui5-radio-button): add parts for overstyling
    JIRA: 3609

--- packages/main/src/RadioButtonTemplate.tsx
+++ packages/main/src/RadioButtonTemplate.tsx
@@ -5,6 +5,7 @@ import Label from "./Label.js";
 export default function RadioButtonTemplate(this: RadioButton) {
 	return (
 		<div
+			part="radio-button-inner"
 			role="radio"
 			class="ui5-radio-root"
 			aria-checked={this.checked}
@@ -19,11 +20,11 @@ export default function RadioButtonTemplate(this: RadioButton) {
 			onMouseUp={this._onmouseup}
 			onFocusOut={this._onfocusout}
 		>
-			<div class={{
+			<div part="ring" class={{
 				"ui5-radio-inner": true,
 				"ui5-radio-inner--hoverable": !this.disabled && !this.readonly && isDesktop(),
 			}}>
-				<svg class="ui5-radio-svg" focusable="false" aria-hidden="true">
+				<svg part="svg" class="ui5-radio-svg" focusable="false" aria-hidden="true">
 					<circle part="outer-ring" class="ui5-radio-svg-outer" cx="50%" cy="50%" r="50%" />
 					<circle part="inner-ring" class="ui5-radio-svg-inner" cx="50%" cy="50%" />
 				</svg>
@@ -39,7 +40,7 @@ export default function RadioButtonTemplate(this: RadioButton) {
 			</div>
 
 			{this.text &&
-				<Label id={`${this._id}-label`} class="ui5-radio-label" for={this._id} wrappingType={this.wrappingType}>{this.text}</Label>
+				<Label part="label" id={`${this._id}-label`} class="ui5-radio-label" for={this._id} wrappingType={this.wrappingType}>{this.text}</Label>
 			}
 
 			{this.hasValueState &&
