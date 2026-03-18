937a4b02e53df5cbfd641fd0b1384f4c6484ab80
Georgi Damyanov <georgi.damyanov@sap.com>
2026-03-18 16:25:37 +0200

    test: add test suite for mobile

--- packages/main/cypress/support/commands/DateRangePicker.commands.ts
+++ packages/main/cypress/support/commands/DateRangePicker.commands.ts
@@ -7,14 +7,40 @@ Cypress.Commands.add("ui5DateRangePickerOpen", { prevSubject: true }, (subject: 
 	cy.wrap(subject).ui5DateRangePickerExpectToBeOpen()
 });
 
+Cypress.Commands.add("ui5DateRangePickerGetPopover", { prevSubject: true }, (subject: JQuery<DateRangePicker>) => {
+	return cy.wrap(subject)
+		.shadow()
+		.find<ResponsivePopover>("[ui5-responsive-popover]");
+});
+
 Cypress.Commands.add("ui5DateRangePickerExpectToBeOpen", { prevSubject: true }, (subject: JQuery<DateRangePicker>) => {
 	cy.wrap(subject)
 		.should("have.prop", "open", true);
 
 	cy.wrap(subject)
-		.shadow()
-		.find<ResponsivePopover>("[ui5-responsive-popover]")
+		.ui5DateRangePickerGetPopover()
 		.ui5ResponsivePopoverOpened();
+});
+
+Cypress.Commands.add("ui5DateRangePickerSelectRange", { prevSubject: true }, (subject: JQuery<DateRangePicker>, startIndex: number, endIndex?: number) => {
+	cy.wrap(subject)
+		.shadow()
+		.find("[ui5-calendar]")
+		.shadow()
+		.find("[ui5-daypicker]")
+		.shadow()
+		.find(".ui5-dp-root .ui5-dp-content div > .ui5-dp-item")
+		.as("dateItems");
+
+	cy.get("@dateItems")
+		.eq(startIndex)
+		.realClick();
+
+	if (endIndex) {
+		cy.get("@dateItems")
+		.eq(endIndex)
+		.realClick();
+	}
 });
 
 declare global {
@@ -23,8 +49,16 @@ declare global {
 			ui5DateRangePickerOpen(
 				this: Chainable<JQuery<DateRangePicker>>,
 			): Chainable<void>;
+			ui5DateRangePickerGetPopover(
+				this: Chainable<JQuery<DateRangePicker>>,
+			): Chainable<JQuery<ResponsivePopover>>;
 			ui5DateRangePickerExpectToBeOpen(
 				this: Chainable<JQuery<DateRangePicker>>,
+			): Chainable<void>;
+			ui5DateRangePickerSelectRange(
+				this: Chainable<JQuery<DateRangePicker>>,
+				startIndex: number,
+				endIndex?: number,
 			): Chainable<void>;
 		}
 	}
