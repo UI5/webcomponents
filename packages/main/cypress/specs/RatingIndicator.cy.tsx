181c17f49dffd1513ef025f427dbf82f0115188d
Martin R. Hristov <m.hristov@sap.com>
2026-05-11 16:38:10 +0300

    feat(ui5-rating-indicator): add halfUnratedIcon property for accessible half-star display
    In read-only/disabled mode, the half-star right side and unselected
    stars now use outline icons instead of filled icons differentiated
    only by color. This improves accessibility for color-blind users.
    
    A new `halfUnratedIcon` property allows explicit control over the
    icon used for the non-selected half. When not set, it falls back
    to `unratedIcon` for backward compatibility with custom icons.
    
    BGSOFUIRILA-4413

--- packages/main/cypress/specs/RatingIndicator.cy.tsx
+++ packages/main/cypress/specs/RatingIndicator.cy.tsx
@@ -85,7 +85,35 @@ describe("RatingIndicator", () => {
 				.should("have.attr", "name", "heart-2");
 		});
 
-		it("should render custom icon (filled) in half-star state when readonly", () => {
+		it("should render halfUnratedIcon in half-star state when readonly", () => {
+			cy.mount(<RatingIndicator value={2.5} ratedIcon="heart" unratedIcon="heart-2" halfUnratedIcon="heart-2" readonly></RatingIndicator>);
+
+			cy.get("[ui5-rating-indicator]")
+				.shadow()
+				.find(".ui5-rating-indicator-item-half .ui5-rating-indicator-half-icon-left [ui5-icon]")
+				.should("have.attr", "name", "heart");
+
+			cy.get("[ui5-rating-indicator]")
+				.shadow()
+				.find(".ui5-rating-indicator-item-half .ui5-rating-indicator-half-icon-right [ui5-icon]")
+				.should("have.attr", "name", "heart-2");
+		});
+
+		it("should render halfUnratedIcon in half-star state when disabled", () => {
+			cy.mount(<RatingIndicator value={2.5} ratedIcon="heart" unratedIcon="heart-2" halfUnratedIcon="heart-2" disabled></RatingIndicator>);
+
+			cy.get("[ui5-rating-indicator]")
+				.shadow()
+				.find(".ui5-rating-indicator-item-half .ui5-rating-indicator-half-icon-left [ui5-icon]")
+				.should("have.attr", "name", "heart");
+
+			cy.get("[ui5-rating-indicator]")
+				.shadow()
+				.find(".ui5-rating-indicator-item-half .ui5-rating-indicator-half-icon-right [ui5-icon]")
+				.should("have.attr", "name", "heart-2");
+		});
+
+		it("should fall back to unratedIcon in readonly half-star when halfUnratedIcon is not set", () => {
 			cy.mount(<RatingIndicator value={2.5} ratedIcon="heart" unratedIcon="heart-2" readonly></RatingIndicator>);
 
 			cy.get("[ui5-rating-indicator]")
@@ -96,21 +124,7 @@ describe("RatingIndicator", () => {
 			cy.get("[ui5-rating-indicator]")
 				.shadow()
 				.find(".ui5-rating-indicator-item-half .ui5-rating-indicator-half-icon-right [ui5-icon]")
-				.should("have.attr", "name", "heart");
-		});
-
-		it("should render custom icon (filled) in half-star state when disabled", () => {
-			cy.mount(<RatingIndicator value={2.5} ratedIcon="heart" unratedIcon="heart-2" disabled></RatingIndicator>);
-
-			cy.get("[ui5-rating-indicator]")
-				.shadow()
-				.find(".ui5-rating-indicator-item-half .ui5-rating-indicator-half-icon-left [ui5-icon]")
-				.should("have.attr", "name", "heart");
-
-			cy.get("[ui5-rating-indicator]")
-				.shadow()
-				.find(".ui5-rating-indicator-item-half .ui5-rating-indicator-half-icon-right [ui5-icon]")
-				.should("have.attr", "name", "heart");
+				.should("have.attr", "name", "heart-2");
 		});
 	});
 
--- packages/main/src/RatingIndicatorTemplate.tsx
+++ packages/main/src/RatingIndicatorTemplate.tsx
@@ -52,13 +52,13 @@ function starLi(this: RatingIndicator, star: Star) {
 	} if (this.readonly) {
 		return (
 			<li class="ui5-rating-indicator-item ui5-rating-indicator-item-unsel">
-				<Icon data-ui5-value={star.index} name={this.ratedIcon} />
+				<Icon data-ui5-value={star.index} name={this.unratedIcon} />
 			</li>
 		);
 	} if (this.disabled) {
 		return (
 			<li class="ui5-rating-indicator-item ui5-rating-indicator-item-unsel">
-				<Icon data-ui5-value={star.index} name={this.ratedIcon} />
+				<Icon data-ui5-value={star.index} name={this.unratedIcon} />
 			</li>
 		);
 	}
@@ -70,5 +70,5 @@ function starLi(this: RatingIndicator, star: Star) {
 }
 
 function halfStarIconName(this: RatingIndicator) {
-	return this.disabled || this.readonly ? this.ratedIcon : this.unratedIcon;
+	return this.disabled || this.readonly ? (this.halfUnratedIcon || this.unratedIcon) : this.unratedIcon;
 }
