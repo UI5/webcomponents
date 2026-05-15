45ceff206888535bec81948ba8c39704551ec62a
Stoyan <abvsuxhard@gmail.com>
2026-05-15 09:13:39 +0300

    feat(ui5-button): [PoC] link behaviour in the button

--- packages/main/src/ButtonTemplate.tsx
+++ packages/main/src/ButtonTemplate.tsx
@@ -10,69 +10,110 @@ export default function ButtonTemplate(this: Button, injectedProps?: {
 		ariaValueNow?: number,
 		ariaValueText?: string,
 }) {
+	const content = (<>
+		{ this.icon &&
+			<Icon
+				class="ui5-button-icon"
+				name={this.icon}
+				mode="Decorative"
+				part="icon"
+			/>
+		}
+
+		<span id={`${this._id}-content`} class="ui5-button-text">
+			<bdi>
+				<slot></slot>
+			</bdi>
+		</span>
+
+		{this.endIcon &&
+			<Icon
+				class="ui5-button-end-icon"
+				name={this.endIcon}
+				mode="Decorative"
+				part="endIcon"
+			/>
+		}
+
+		{this.shouldRenderBadge &&
+			<slot name="badge"/>
+		}
+	</>);
+
+	const classes = {
+		"ui5-button-root": true,
+		"ui5-button-badge-placement-end": this.badge[0]?.design === "InlineText",
+		"ui5-button-badge-placement-end-top": this.badge[0]?.design === "OverlayText",
+		"ui5-button-badge-dot": this.badge[0]?.design === "AttentionDot",
+	};
+
 	return (<>
-		<button
-			type="button"
-			class={{
-				"ui5-button-root": true,
-				"ui5-button-badge-placement-end": this.badge[0]?.design === "InlineText",
-				"ui5-button-badge-placement-end-top": this.badge[0]?.design === "OverlayText",
-				"ui5-button-badge-dot": this.badge[0]?.design === "AttentionDot"
-			}}
-			disabled={this.disabled}
-			data-sap-focus-ref
-			aria-pressed={injectedProps?.ariaPressed}
-			aria-valuemin={injectedProps?.ariaValueMin}
-			aria-valuemax={injectedProps?.ariaValueMax}
-			aria-valuenow={injectedProps?.ariaValueNow}
-			aria-valuetext={injectedProps?.ariaValueText}
-			onFocusOut={this._onfocusout}
-			onClick={this._onclick}
-			onMouseDown={this._onmousedown}
-			onKeyDown={this._onkeydown}
-			onKeyUp={this._onkeyup}
-			onTouchStart={this._ontouchstart}
-			onTouchEnd={this._ontouchend}
-			tabindex={this.tabIndexValue}
-			aria-expanded={this._computedAccessibilityAttributes?.expanded}
-			aria-controls={this._computedAccessibilityAttributes?.controls}
-			aria-haspopup={this._computedAccessibilityAttributes?.hasPopup}
-			aria-label={this._computedAccessibilityAttributes?.ariaLabel}
-			aria-keyshortcuts={this._computedAccessibilityAttributes?.ariaKeyShortcuts}
-			aria-description={this.ariaDescriptionText}
-			aria-busy={this.loading ? "true" : undefined}
-			title={this.buttonTitle}
-			part="button"
-			role={this.effectiveAccRole}
-		>
-			{ this.icon &&
-				<Icon
-					class="ui5-button-icon"
-					name={this.icon}
-					mode="Decorative"
-					part="icon"
-				/>
-			}
-
-			<span id={`${this._id}-content`} class="ui5-button-text">
-				<bdi>
-					<slot></slot>
-				</bdi>
-			</span>
-
-			{this.endIcon &&
-				<Icon
-					class="ui5-button-end-icon"
-					name={this.endIcon}
-					mode="Decorative"
-					part="endIcon"
-				/>
-			}
-
-			{this.shouldRenderBadge &&
-				<slot name="badge"/>
-			}
-		</button>
+		{this._isLink ? (
+			<a
+				class={classes}
+				href={this.parsedRef}
+				target={this.target}
+				rel={this._rel}
+				data-sap-focus-ref
+				aria-disabled={this.disabled ? "true" : undefined}
+				aria-pressed={injectedProps?.ariaPressed}
+				aria-valuemin={injectedProps?.ariaValueMin}
+				aria-valuemax={injectedProps?.ariaValueMax}
+				aria-valuenow={injectedProps?.ariaValueNow}
+				aria-valuetext={injectedProps?.ariaValueText}
+				onFocusOut={this._onfocusout}
+				onClick={this._onclick}
+				onMouseDown={this._onmousedown}
+				onKeyDown={this._onkeydown}
+				onKeyUp={this._onkeyup}
+				onTouchStart={this._ontouchstart}
+				onTouchEnd={this._ontouchend}
+				tabindex={this.tabIndexValue}
+				aria-expanded={this._computedAccessibilityAttributes?.expanded}
+				aria-controls={this._computedAccessibilityAttributes?.controls}
+				aria-haspopup={this._computedAccessibilityAttributes?.hasPopup}
+				aria-label={this._computedAccessibilityAttributes?.ariaLabel}
+				aria-keyshortcuts={this._computedAccessibilityAttributes?.ariaKeyShortcuts}
+				aria-description={this.ariaDescriptionText}
+				aria-busy={this.loading ? "true" : undefined}
+				title={this.buttonTitle}
+				part="button"
+			>
+				{content}
+			</a>
+		) : (
+			<button
+				type="button"
+				class={classes}
+				disabled={this.disabled}
+				data-sap-focus-ref
+				aria-pressed={injectedProps?.ariaPressed}
+				aria-valuemin={injectedProps?.ariaValueMin}
+				aria-valuemax={injectedProps?.ariaValueMax}
+				aria-valuenow={injectedProps?.ariaValueNow}
+				aria-valuetext={injectedProps?.ariaValueText}
+				onFocusOut={this._onfocusout}
+				onClick={this._onclick}
+				onMouseDown={this._onmousedown}
+				onKeyDown={this._onkeydown}
+				onKeyUp={this._onkeyup}
+				onTouchStart={this._ontouchstart}
+				onTouchEnd={this._ontouchend}
+				tabindex={this.tabIndexValue}
+				aria-expanded={this._computedAccessibilityAttributes?.expanded}
+				aria-controls={this._computedAccessibilityAttributes?.controls}
+				aria-haspopup={this._computedAccessibilityAttributes?.hasPopup}
+				aria-label={this._computedAccessibilityAttributes?.ariaLabel}
+				aria-keyshortcuts={this._computedAccessibilityAttributes?.ariaKeyShortcuts}
+				aria-description={this.ariaDescriptionText}
+				aria-busy={this.loading ? "true" : undefined}
+				title={this.buttonTitle}
+				part="button"
+				role={this.effectiveAccRole}
+			>
+				{content}
+			</button>
+		)}
 		{this.loading &&
 			<BusyIndicator
 				id={`${this._id}-button-busy-indicator`}
