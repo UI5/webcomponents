c2b600ececec6dcc5e7a01267b4bb73ba9a12fde
Nayden Naydenov <nnaydenow.work@gmail.com>
2026-05-21 11:34:17 +0300

    feat(ui5-icon): add symbol slot for font-based icon libraries

--- packages/main/src/IconTemplate.tsx
+++ packages/main/src/IconTemplate.tsx
@@ -1,6 +1,24 @@
 import type Icon from "./Icon.js";
 
 export default function IconTemplate(this: Icon) {
+	if (this.hasSymbol) {
+		return (
+			<span
+				class="ui5-icon-root"
+				part="root"
+				tabindex={this._tabIndex}
+				role={this.effectiveAccessibleRole}
+				aria-label={this.effectiveAccessibleName}
+				aria-hidden={this.effectiveAriaHidden}
+				onKeyDown={this._onkeydown}
+				onKeyUp={this._onkeyup}
+				onClick={this._onclick}
+			>
+				<slot name="symbol"></slot>
+			</span>
+		);
+	}
+
 	return (
 		<svg
 			class="ui5-icon-root"
