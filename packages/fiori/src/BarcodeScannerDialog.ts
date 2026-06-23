bf71ec8c1695fbe30a4b53eeaeea236d77dde77e
Nikola Anachkov <nikola.anachkov@sap.com>
2026-06-23 15:25:22 +0300

    fix: address comment

--- packages/fiori/src/BarcodeScannerDialog.ts
+++ packages/fiori/src/BarcodeScannerDialog.ts
@@ -44,24 +44,7 @@ const defaultMediaConstraints = {
 	},
 };
 
-type BarcodeFormatString =
-	| "AZTEC"
-	| "CODABAR"
-	| "CODE_39"
-	| "CODE_93"
-	| "CODE_128"
-	| "DATA_MATRIX"
-	| "EAN_8"
-	| "EAN_13"
-	| "ITF"
-	| "MAXICODE"
-	| "PDF_417"
-	| "QR_CODE"
-	| "RSS_14"
-	| "RSS_EXPANDED"
-	| "UPC_A"
-	| "UPC_E"
-	| "UPC_EAN_EXTENSION";
+type BarcodeFormatString = keyof typeof BarcodeFormat;
 
 type BarcodeScannerDialogScanSuccessEventDetail = {
 	text: string,
