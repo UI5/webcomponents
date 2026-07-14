41e5d6b9a04ef23153b84471d13dd18dde0a9cef
Lukas Harbarth <lukas.harbarth@sap.com>
2026-07-14 12:31:14 +0200

    remove duplicate Note

--- packages/main/src/Toolbar.ts
+++ packages/main/src/Toolbar.ts
@@ -160,7 +160,7 @@ class Toolbar extends UI5Element {
 	/**
 	 * Defines the items of the component.
 	 *
-	 * **Note:** **Note:** Use `ui5-toolbar-button`, `ui5-toolbar-select`, `ui5-toolbar-separator` and `ui5-toolbar-spacer` for the most common toolbar actions. To place any other UI5 Web Component into the toolbar and have it participate in overflow handling, wrap it in a `ui5-toolbar-item`.
+	 * **Note:** Use `ui5-toolbar-button`, `ui5-toolbar-select`, `ui5-toolbar-separator` and `ui5-toolbar-spacer` for the most common toolbar actions. To place any other UI5 Web Component into the toolbar and have it participate in overflow handling, wrap it in a `ui5-toolbar-item`.
 	 * @public
 	 */
 	@slot({
