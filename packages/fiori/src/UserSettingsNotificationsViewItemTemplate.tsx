7f2e3935648ea0516e834e00961df7ac3fa9ed84
Dobromira Boycheva <dobromira.boycheva@sap.com>
2026-08-03 11:34:03 +0300

    feat(ui5-user-settings-notifications-view): introduce NotificationsView component family

---
+++ packages/fiori/src/UserSettingsNotificationsViewItemTemplate.tsx
@@ -0,0 +1,50 @@
+import type UserSettingsNotificationsViewItem from "./UserSettingsNotificationsViewItem.js";
+import ListItemCustomTemplate from "@ui5/webcomponents/dist/ListItemCustomTemplate.js";
+import Switch from "@ui5/webcomponents/dist/Switch.js";
+import Icon from "@ui5/webcomponents/dist/Icon.js";
+import slimArrowRight from "@ui5/webcomponents-icons/dist/slim-arrow-right.js";
+
+export default function UserSettingsNotificationsViewItemTemplate(this: UserSettingsNotificationsViewItem) {
+	return ListItemCustomTemplate.call(this, {
+		listItemContent: listItemContent.bind(this),
+	});
+}
+
+function listItemContent(this: UserSettingsNotificationsViewItem) {
+	const rootClasses = ["ui5-user-settings-notifications-item"];
+	if (this.bylineText && this.text) {
+		rootClasses.push("has-byline");
+	}
+
+	return (
+		<div class={rootClasses.join(" ")}>
+			<div class="ui5-user-settings-notifications-item-start">
+				<div class="ui5-user-settings-notifications-item-texts">
+					{this.text &&
+						<span class="ui5-user-settings-notifications-item-title">{this.text}</span>
+					}
+					{this.bylineText &&
+						<span class="ui5-user-settings-notifications-item-byline">{this.bylineText}</span>
+					}
+				</div>
+			</div>
+			<div class="ui5-user-settings-notifications-item-end" onClick={this._handleEndClick}>
+				{this._hasEndContent
+					? <slot name="endContent"></slot>
+					: <Switch
+						class="ui5-user-settings-notifications-item-switch"
+						checked={this.checked}
+						onChange={this._handleSwitchChange}
+						accessibleName={this._accessibleSwitchName}
+					></Switch>
+				}
+				{this.navigable &&
+					<Icon
+						class="ui5-user-settings-notifications-item-arrow"
+						name={slimArrowRight}
+					></Icon>
+				}
+			</div>
+		</div>
+	);
+}
