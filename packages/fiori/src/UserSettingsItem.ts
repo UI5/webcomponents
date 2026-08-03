a3bbb826f67a7922e47ce9b90ac8a19ed24e44e8
Dobromira Boycheva <dobromira.boycheva@sap.com>
2026-08-03 11:34:03 +0300

    feat(ui5-user-settings-notifications-view): introduce NotificationsView component family

--- packages/fiori/src/UserSettingsItem.ts
+++ packages/fiori/src/UserSettingsItem.ts
@@ -312,6 +312,14 @@ class UserSettingsItem extends UI5Element {
 		focusable?.focus();
 	}
 
+	/**
+	 * @private
+	 */
+	_focusBackButton() {
+		const backButton = this.shadowRoot?.querySelector<HTMLElement>(".ui5-user-settings-item-collapse-btn");
+		backButton?.focus();
+	}
+
 	captureRef(this: UserSettingsView, ref: HTMLElement & { associatedSettingView?: UserSettingsView} | null) {
 		if (ref) {
 			ref.associatedSettingView = this;
