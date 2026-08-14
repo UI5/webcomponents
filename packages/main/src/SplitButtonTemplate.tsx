ae867efb4a4a9d58d50abeb1b8fd900926af5b9d
Todor Stoyanov <t.stoyanov@sap.com>
2026-08-14 11:55:26 +0300

    fix(ui5-split-button): announce accessible name before instructions

--- packages/main/src/SplitButtonTemplate.tsx
+++ packages/main/src/SplitButtonTemplate.tsx
@@ -10,8 +10,7 @@ export default function SplitButtonTemplate(this: SplitButton) {
 			tabindex={this._tabIndex}
 			aria-labelledby={!this._hideArrowButton ? `${this._id}-invisibleTextDefault ${this._id}-invisibleText` : undefined}
 			aria-haspopup={this._computedAccessibilityAttributes?.root?.hasPopup}
-			aria-roledescription={this._computedAccessibilityAttributes?.root?.roleDescription}
-			aria-label={this._computedAccessibilityAttributes?.root?.title}
+aria-label={this._computedAccessibilityAttributes?.root?.title}
 			aria-keyshortcuts={this._computedAccessibilityAttributes?.root?.ariaKeyShortcuts}
 			onFocusOut={this._onFocusOut}
 			onKeyDown={this._onKeyDown}
