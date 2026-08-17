640731c2641f6e7f800d870fffc93026aa3f20a3
Nayden Naydenov <nnaydenow.work@gmail.com>
2026-08-17 14:05:25 +0300

    chore: bump dependencies

--- packages/tools/lib/cem/custom-elements-manifest.config.mjs
+++ packages/tools/lib/cem/custom-elements-manifest.config.mjs
@@ -24,7 +24,7 @@ import {
 	displayDocumentationErrors,
 	toKebabCase
 } from "./utils.mjs";
-import { generateCustomData } from "cem-plugin-vs-code-custom-data-generator";
+import { generateVsCodeCustomElementData } from "custom-element-vs-code-integration";
 import { customElementJetBrainsPlugin } from "custom-element-jet-brains-integration";
 
 const packageJSON = JSON.parse(fs.readFileSync("./package.json"));
@@ -628,7 +628,7 @@ export default {
 				});
 			}
 		},
-		wrapPluginForQuietMode(generateCustomData({ outdir: "dist", cssFileName: null, cssPropertiesDocs: false })),
+		wrapPluginForQuietMode(generateVsCodeCustomElementData({ outdir: "dist", cssFileName: null, cssPropertiesDocs: false })),
 		wrapPluginForQuietMode(customElementJetBrainsPlugin({ outdir: "dist", cssFileName: null, cssPropertiesDocs: false }))
 	],
 };
