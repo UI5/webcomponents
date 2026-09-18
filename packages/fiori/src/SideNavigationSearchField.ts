055afb034497ab7a47b93a88757fbe6b4ed1e1c7
Teodor Taushanov <teodor.taushanov@sap.com>
2026-09-18 11:13:35 +0300

    chore: add default placeholder

--- packages/fiori/src/SideNavigationSearchField.ts
+++ packages/fiori/src/SideNavigationSearchField.ts
@@ -1,8 +1,11 @@
 import customElement from "@ui5/webcomponents-base/dist/decorators/customElement.js";
+import i18n from "@ui5/webcomponents-base/dist/decorators/i18n.js";
 import jsxRenderer from "@ui5/webcomponents-base/dist/renderer/JsxRenderer.js";
+import type I18nBundle from "@ui5/webcomponents-base/dist/i18nBundle.js";
 import SearchField from "./SearchField.js";
 import SearchFieldTemplate from "./SearchFieldTemplate.js";
 import SideNavigationSearchFieldCss from "./generated/themes/SideNavigationSearchField.css.js";
+import { SIDE_NAVIGATION_SEARCH_FIELD_PLACEHOLDER } from "./generated/i18n/i18n-defaults.js";
 
 /**
  * @class
@@ -30,6 +33,12 @@ import SideNavigationSearchFieldCss from "./generated/themes/SideNavigationSearc
 	],
 })
 class SideNavigationSearchField extends SearchField {
+	@i18n("@ui5/webcomponents-fiori")
+	static i18nBundle: I18nBundle;
+
+	get _effectivePlaceholder(): string | undefined {
+		return this.placeholder || SideNavigationSearchField.i18nBundle.getText(SIDE_NAVIGATION_SEARCH_FIELD_PLACEHOLDER);
+	}
 }
 
 SideNavigationSearchField.define();
